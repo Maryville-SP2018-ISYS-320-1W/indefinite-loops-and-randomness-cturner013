356698225e2e202214118a6a236e2cca1533a7c4
Cturner013 <cturner7@live.maryville.edu>
2018-04-28 15:47:20 -0500

    Loops

---
+++ src/Flipper.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Flipper {
+	public static void main (String[] arges) {
+		int a =0;
+		String head = "H";
+		String tail = "T";
+		int num = 1;
+		int totalCount = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Stop when we flip how many heads consecutively?");
+		int count = input.nextInt();
+		int headCount = 0;
+		while(headCount !=count) {
+			totalCount++;
+			if(num>=1&&num%5==0) {
+				System.out.println();
+				num=1;
+			}else {
+				num++;
+			}
+			Random rand = new Random();
+			a = rand.nextInt (2);
+			if(a==0) {
+				System.out.print("H");
+				headCount++;
+			}
+			if(a==1) {
+				System.out.print("T");
+				headCount=0;
+			}
+		}
+		System.out.println("\nWe did it in "+totalCount+"flips.");
+			}
+		
+	}
+
+
--- src/P4_DoLoopPredictions.java
+++ src/P4_DoLoopPredictions.java
@@ -11,33 +11,61 @@
  What do you expect the loop to output?
  
  (a)
- Repeats:
- Expected output:
+ Repeats:10
+ Expected output:91
  (b)
- Repeats:
- Expected output:
+ Repeats:Infinity
+ Expected output: - numbers
  (c)
- Repeats:
- Expected output:
+ Repeats:Infinity
+ Expected output:250
  (d)
- Repeats:
- Expected output:
+ Repeats:0
+ Expected output:0
    
  */
 
 /*
  Was your prediction correct?
  
- (a)
- (b)
- (c)
- (d)
+ (a) Correct
+ (b)Correct
+ (c) Correct
+ (d)Correct
 
 */
 public class P4_DoLoopPredictions {
 
 	public static void main(String[] args) {
 		System.out.println("STARTING LOOP TEST");
+		System.out.println("STARTING LOOP TEST");
+		int x = 1;
+		while(x < 100) {
+			System.out.print(x + "");
+			x += 10;
+			
+		}
+		int max = 10;
+		while(max < 10) {
+			System.out.println("count down); " + max);
+			max--;
+			
+		}
+		int y = 250;
+		while(y % 3 != 0) {
+			System.out.println(y);
+		}
+		String word = "a";
+		while(word.length()<10) {
+			word= "b"+ word + "b";
+		}
+		System.out.println(word);
+		
+
+		// copy and paste the loop to test between here, between the println statements
+		
+				
+		System.out.println("ENDING LOOP TEST");
 		
 		
 		// copy and paste the loop to test between here, between the println statements
